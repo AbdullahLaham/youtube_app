2c69fed23bf1854df30f6af3e6312ca6dff4cc48
AbdullahLaham <abed26194@gmail.com>
2022-09-11 13:30:16 +0300

    create-file-structure

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Menu from './Menu';
+const Layout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Layout
